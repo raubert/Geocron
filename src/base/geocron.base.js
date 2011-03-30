c922c7ef382107ad3b626f14c877dcb3d71a81f2
"Raphaël Aubert" <raphael.aubert@gmail.com>
2011-03-29 22:40:57 -0400

    faction / alliance / ally recovery improved

--- src/base/geocron.base.js
+++ src/base/geocron.base.js
@@ -6,17 +6,12 @@
 /**
  * Script version; for reference purpose.
  */
-GEOCRON.version = '@VERSION';
-
-/**
- * Tools container.
- */
-GEOCRON.container = null;
+var version = '@VERSION',
 
 /**
  * All modules will be pushed here.
  */
-GEOCRON.modules = [];
+modules = [],
 
 /**
  * Logging utility based on what's available.
@@ -24,33 +19,12 @@ GEOCRON.modules = [];
  * @param message
  *            anything you want to log
  */
-GEOCRON.log = typeof console != 'undefined' ? function( message ) { console.log( message ); } : function() {};
+log = typeof console != 'undefined' ? function( message ) { console.log( message ); } : function() {},
 
 /**
- * Main function: initializes all registered modules; this has to be called *after* all modules have been registered.
+ * Contains the user faction.
  */
-GEOCRON.initialize = function() {
-
-	var start = new Date().getTime();
-	GEOCRON.log( 'GEOCRON: launching script v' + GEOCRON.version + '...' );
-
-	// then initialize the modules
-	for ( var i = 0; i < GEOCRON.modules.length; i++ ) {
-		GEOCRON.log( 'GEOCRON: initializing "' + GEOCRON.modules[i].name + '" module...' );
-		try {
-			if ( GEOCRON.modules[i].initialize() === false ) {
-				GEOCRON.log( 'GEOCRON: Module "' + GEOCRON.modules[i].name + '" not active on this page.' );
-			} else {
-				GEOCRON.log( 'GEOCRON: Module "' + GEOCRON.modules[i].name + '" ready.' );
-			}
-		} catch( e ) {
-			GEOCRON.log( 'GEOCRON: Module "' + GEOCRON.modules[i].name + '" failed!' );
-			GEOCRON.log( e );
-		}
-	}
-	GEOCRON.log( 'GEOCRON: initialization complete [' + ( new Date().getTime() - start ) + 'ms].' );
-
-};
+faction = null;
 
 /**
  * Pretty print for numbers using space as 1K separator.
@@ -58,7 +32,7 @@ GEOCRON.initialize = function() {
  * @param {Number} number
  *            the value to format
  */
-GEOCRON.formatNumber = function( number ) {
+function formatNumber( number ) {
 	if ( !number ) {
 		return number;
 	}
@@ -77,10 +51,89 @@ GEOCRON.formatNumber = function( number ) {
 		ret = rep;
 	}
 	return ret;
+}
+
+/**
+ * Tries to guess the user faction based on some mouseover function code.
+ * 
+ * @return {String} the user faction
+ */
+function getFaction() {
+	if ( faction == null ) {
+		faction = $( 'table.second:first td:nth(2) a:nth(2)' ).attr( 'onmouseover' ).toString().replace( /.*\n.*Faction : ([^<]+)<.*\n.*/, '$1' );
+	}
+	return faction;
+}
+
+function getAlliance( faction ) {
+	switch( faction ) {
+	case 'E.R.E.L':
+	case 'F.G.V':
+	case 'O.S.G':
+		return 'PF';
+	case 'C.C.K':
+	case 'G.D':
+	case 'R.P':
+		return 'MOO';
+	case 'F.M.U.S':
+	case 'P.M':
+		return 'PMS';
+	default:
+		return null;
+	}
+}
+
+/**
+ * Check whether the specified 'other' faction is an ally or not.
+ * 
+ * @param {String} other
+ *            the faction to check
+ * @return true if the faction is an ally; else false
+ */
+function isAlly( other ) {
+	return getAlliance( getFaction() ) == getAlliance( other );
+}
+
+/**
+ * Main function: initializes all registered modules; this has to be called *after* all modules have been registered.
+ */
+function initialize() {
+
+	var start = new Date().getTime();
+	log( 'GEOCRON: launching script v' + version + '...' );
+
+	// then initialize the modules
+	for ( var i = 0; i < modules.length; i++ ) {
+		log( 'GEOCRON: initializing "' + modules[i].name + '" module...' );
+		try {
+			if ( modules[i].initialize() === false ) {
+				log( 'GEOCRON: Module "' + modules[i].name + '" not active on this page.' );
+			} else {
+				log( 'GEOCRON: Module "' + modules[i].name + '" ready.' );
+			}
+		} catch( e ) {
+			log( 'GEOCRON: Module "' + modules[i].name + '" failed!' );
+			log( e );
+		}
+	}
+	log( 'GEOCRON: initialization complete [' + ( new Date().getTime() - start ) + 'ms].' );
+
 };
 
-GEOCRON.isAlly = function( faction ) {
-	return faction == 'F.G.V' || faction == 'E.R.E.L' || faction == 'O.S.G';
-};
+/**
+ * Expose some public methods and attributes.
+ */
+$.extend(GEOCRON, {
+
+	formatNumber: formatNumber,
+
+	getFaction: getFaction,
+	getAlliance: getAlliance,
+	isAlly: isAlly,
+
+	modules: modules,
+	initialize: initialize
+
+});
 
 })( GEOCRON.jQuery, GEOCRON );
